3234b0dd8905e4c454474471b163286620bd7424
PhantomNoir <evalopez359@gmail.com>
2025-11-30 04:47:35 -0600

    Set up logic to connect chat client and handle logic for when users send a chat that starts with !song

---
+++ src/twitch/chatClient.js
@@ -0,0 +1,39 @@
+const { ChatClient } = require('@twurple/chat');
+const { RefreshingAuthProvider } = require('@twurple/auth');
+const fs = require('fs');
+const { handleSongRequest } = require('../handlers/songRequestHandler');
+const { channel } = require('diagnostics_channel');
+
+let chatClient;
+
+async function startChatClient() {
+    const clientId = process.env.TWITCH_CLIENT_ID;
+    const clientSecret = process.env.TWITCH_CLIENT_SECRET;
+
+    const tokenData = JSON.parse(fs.readFileSync('./tokens.json', 'utf-8'));
+
+    const authProvider = new RefreshingAuthProvider(
+        {
+            clientId,
+            clientSecret,
+            onRefresh: newTokenData => fs.writeFileSync('./token.json', JSON.stringify(newTokenData, null, 4))
+        },
+        tokenData
+    );
+
+    chatClient = new ChatClient({
+        authProvider,
+        channels: [process.env.TWITCH_CHANNEL]
+    });
+
+    chatClient.onMessage(async (channel, user, message) => {
+        if (message.startsWith('!song')) {
+            await handleSongRequest(user, message);
+        }
+    });
+
+    await chatClient.connect();
+    console.log('Chat client connected.');
+}
+
+module.exports = { startChatClient };
