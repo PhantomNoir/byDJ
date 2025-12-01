309ba9e4ad38da40a907ea0b6cc59745e800b304
PhantomNoir <evalopez359@gmail.com>
2025-12-01 02:00:10 -0600

    Implemented express server logic and OAuth logic for Spotify.

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 require('dotenv').config();
+const express = require('express')
+const path = require('path');
+
 const { startChatClient } = require('./src/twitch/chatClient');
 const { initEventSub } = require('./src/twitch/eventSub');
+const spotifyAuth = require('./src/server/spotifyAuth');
 
-(async () => {
-    console.log('Starting byDJ, let\'s get funky!');
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-    await startChatClient();
-    await initEventSub();
-})();
+app.use(express.json());
+app.use('/', spotifyAuth);
+
+app.get('/', (req, res) => res.send('byDJ server running'));
+
+app.listen(PORT, async () => {
+    console.log(`HTTP server listening on http://127.0.0.1:${PORT}`);
+
+    try {
+        await startChatClient();
+        await initEventSub();
+        console.log('Twitch clients started.')
+    } catch (err) {
+        console.error('Failed to start Twitch clients:', err);
+    }
+});
