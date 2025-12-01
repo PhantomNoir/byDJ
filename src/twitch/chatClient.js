1884d03892cba5f8ab2ba144c9a7b7eabdc3308c
PhantomNoir <evalopez359@gmail.com>
2025-12-01 02:31:42 -0600

    Corrected pathing for token.json

--- src/twitch/chatClient.js
+++ src/twitch/chatClient.js
@@ -4,19 +4,23 @@ const fs = require('fs');
 const { handleSongRequest } = require('../handlers/songRequestHandler');
 const { channel } = require('diagnostics_channel');
 
+const path = require('path');
+const tokenPath = path.join(__dirname, '../../tokens.json');
+
 let chatClient;
 
 async function startChatClient() {
     const clientId = process.env.TWITCH_CLIENT_ID;
     const clientSecret = process.env.TWITCH_CLIENT_SECRET;
 
-    const tokenData = JSON.parse(fs.readFileSync('./tokens.json', 'utf-8'));
+    const tokenData = JSON.parse(fs.readFileSync(tokenPath, 'utf-8'));
 
     const authProvider = new RefreshingAuthProvider(
         {
             clientId,
             clientSecret,
-            onRefresh: newTokenData => fs.writeFileSync('./token.json', JSON.stringify(newTokenData, null, 4))
+            onRefresh: newTokenData => 
+                fs.writeFileSync(tokenPath, JSON.stringify(newTokenData, null, 4))
         },
         tokenData
     );
--- src/twitch/eventSub.js
+++ src/twitch/eventSub.js
@@ -4,18 +4,22 @@ const { RefreshingAuthProvider } = require('@twurple/auth');
 const fs = require('fs');
 const { handleSongRequest } = require('../handlers/songRequestHandler');
 
+const path = require('path');
+const tokenPath = path.join(__dirname, '../../tokens.json');
+
 async function initEventSub() {
     const clientId = process.env.TWITCH_CLIENT_ID;
     const clientSecret = process.env.TWITCH_CLIENT_SECRET;
     const channelId = process.env.TWITCH_CHANNEL_ID;
 
-    const tokenData = JSON.parse(fs.readFileSync('./tokens.json', 'utf-8'));
+    const tokenData = JSON.parse(fs.readFileSync(tokenPath, 'utf-8'));
 
     const authProvider = new RefreshingAuthProvider(
         {
             clientId,
             clientSecret,
-            onRefresh: newTokenData => fs.writeFileSync('./tokens.json', JSON.stringify(newTokenData, null, 4))
+            onRefresh: newTokenData => 
+                fs.writeFileSync(tokenPath, JSON.stringify(newTokenData, null, 4))
         },
         tokenData
     );
